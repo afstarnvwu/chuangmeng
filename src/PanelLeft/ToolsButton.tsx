d4f0cc70e950c8dddc2e601b9cb02d5c9fe5012c
AFStar <afstar@163.com>
2023-12-11 17:11:35 +0800

    button修改2

--- src/PanelLeft/ToolsButton.tsx
+++ src/PanelLeft/ToolsButton.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './ToolsButton.less';
 
 function ToolsButton() {
-  return (
-    <div className="tools-button">
-        <span className='button-text'>文本</span>
-    </div>
-  );
+    const [isActive, setIsActive] = useState(false);
+
+    const handleClick = () => {
+        setIsActive(!isActive);
+    }
+    return (
+        <div className={`tools-button ${isActive ? 'active' : 'before'}`} onClick={handleClick}>
+            <span></span>
+            <span className='button-text'>文本</span>
+        </div>
+    );
 }
 
 export default ToolsButton;
