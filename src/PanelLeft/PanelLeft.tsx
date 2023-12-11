95c9f76f95fdee6f848850594d5a3b6200902b46
AFStar <afstar@163.com>
2023-12-11 21:26:23 +0800

    button第三改

--- src/PanelLeft/PanelLeft.tsx
+++ src/PanelLeft/PanelLeft.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from '../images/logo.svg';
 import './PanelLeft.less';
-import ToolsButton from './ToolsButton';
+import { default as ToolsButton } from './ToolsButton';
 
 function PanelLeft() {
   return (
     <div className="left-panel">
-      <ToolsButton/>
+      <div style={{ height: '50px' }} />
+      <ToolsButton imageUrl={logo} />
     </div>
   );
 }
--- src/PanelLeft/ToolsButton.tsx
+++ src/PanelLeft/ToolsButton.tsx
@@ -1,17 +1,21 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
+import logo from '../images/logo.svg';
 import './ToolsButton.less';
 
-function ToolsButton() {
+interface ButtonProps {
+    imageUrl: string;
+}
+function ToolsButton(imageUrl: ButtonProps) {
     const [isActive, setIsActive] = useState(false);
 
     const handleClick = () => {
         setIsActive(!isActive);
     }
     return (
-        <div className={`tools-button ${isActive ? 'active' : 'before'}`} onClick={handleClick}>
-            <span></span>
+        <div className='tools'>
+            <img className='button-image' src={imageUrl.imageUrl} alt="Button Image" />
             <span className='button-text'>文本</span>
+            <span className={`tools-button ${isActive ? 'active' : 'before'}`} onClick={handleClick}></span>
         </div>
     );
 }
